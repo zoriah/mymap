24a05e396fc8c7ab700e091624a2ca119fbc8647
zoriah <noa351@hotmail.de>
2026-06-26 18:25:19 +0200

    tested if still works: yes it dose :)

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# React + Vite
+# Provide a functional API-Key from OpenWetherMap:
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+##
 
-Currently, two official plugins are available:
+### This is a Webapp made in the End of 2024 and is since then not maintained anymore!
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+##
+
+**1. Generate File named .env:**
+
+**2. Put in:**
+
+- VITE_APIKEY=**YOURE APIKEY**
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,9 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import {
-    MapContainer,
-    TileLayer,
-    Marker,
-    Popup
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
 } from 'react-leaflet'
 import L from 'leaflet';
 import "./map.css"
@@ -22,24 +22,16 @@ const location = (cityName) => new L.Icon({
   iconAnchor: calculateIconAnchor(cityName)
 });
 
-// const cities = [
-//   { name: "berlin", lat: "52.520008", lon: "13.404954" },
-//   { name: "london", lat: "51.507351", lon: "-0.127758" },
-//   { name: "paris", lat: "48.856613", lon: "2.352222" },
-//   { name: "washingtondc", lat: "38.892059", lon: "-77.019913" },
-//   { name: "ankara", lat: "39.925533", lon: "32.866287" },
-// ]
-
 const APIKEY = import.meta.env.VITE_APIKEY;
 
 const Map = () => {
-  let selected 
-  
+  let selected
+
   const [loading, setLoading] = useState(false)
   const [city, setCity] = useState([])
   const [center, setCenter] = useState([52.520008, 13.404954])
-  const [selectedCity, setSelectedCity]=useState("berlin")
-  
+  const [selectedCity, setSelectedCity] = useState("berlin")
+
   // const wetherData = async () => {
   //   try {
   //     await axios.get()
@@ -58,7 +50,7 @@ const Map = () => {
     selected = document.getElementById("selection")
     // console.log(selected)
     // console.log("value selectedCity: "+selectedCity)
-    
+
     const fetchCity = async () => {
       setLoading(true);
       try {
@@ -72,9 +64,9 @@ const Map = () => {
         console.error(error);
       } finally {
         setLoading(false);
-      }      
+      }
     };
-    fetchCity()  
+    fetchCity()
   }, [selectedCity]);
 
   // city.length === 0 ?
@@ -82,35 +74,35 @@ const Map = () => {
   //   console.log(city)
 
   return (
-    <>      
+    <>
       <h1 className='fontmed'> Das Wetter in</h1>
       <select id="selection" onChange={e => {
         console.log(e.target.value)
-        setSelectedCity(e.target.value)        
+        setSelectedCity(e.target.value)
       }} className='select' name="cities">
-          <option value="berlin">Berlin</option>
-          <option value="london">London</option>
-          <option value="paris">Paris</option>
-          <option value="ankara">Ankara</option>
-      </select>        
-      <h1 style={{marginLeft:"1rem"}} className='fontmed'>(Für Details klick das unten gezeigte Standortsymbol)</h1>
+        <option value="berlin">Berlin</option>
+        <option value="london">London</option>
+        <option value="paris">Paris</option>
+        <option value="ankara">Ankara</option>
+      </select>
+      <h1 style={{ marginLeft: "1rem" }} className='fontmed'>(Für Details klick das unten gezeigte Standortsymbol)</h1>
       {city.length === 0 ?
-        (<p>Loading</p>):(
-      <MapContainer className='container-map' center={center} zoom={13} key={JSON.stringify(center)}>
-          <TileLayer
+        (<p>Loading</p>) : (
+          <MapContainer className='container-map' center={center} zoom={13} key={JSON.stringify(center)}>
+            <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        <Marker position={[city.coord.lat, city.coord.lon]} icon={location(selectedCity)}>
+            />
+            <Marker position={[city.coord.lat, city.coord.lon]} icon={location(selectedCity)}>
               <Popup>
-                <h1>Atuell: { Math.trunc(city.main.temp)+"°C"}</h1>
+                <h1>Atuell: {Math.trunc(city.main.temp) + "°C"}</h1>
                 <h2>min: {Math.trunc(city.main.temp_min) + "°C"}</h2>
-                <h2>max: { Math.trunc(city.main.temp_max)+"°C"}</h2>
+                <h2>max: {Math.trunc(city.main.temp_max) + "°C"}</h2>
               </Popup>
-          </Marker>
-        </MapContainer>
-      )}
-  </>
+            </Marker>
+          </MapContainer>
+        )}
+    </>
   )
 }
 
